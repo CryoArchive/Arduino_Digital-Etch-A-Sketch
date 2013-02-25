bdcd5ac5bcf94559d568882505b82571002950ec
CryoArchive <CryoArchive@gmail.com>
2013-02-25 15:28:10 +0300

    Digital Etch a Sketch
    This is code for processing and arduino to interface with rotary
    encoders and the arduino mega.

---
+++ Code/Etch_a_Sketch_Pad/Etch_a_Sketch_Pad.pde
@@ -0,0 +1,50 @@
+import processing.serial.*;
+Serial myPort;        // The serial port
+
+int pulses;
+int NpulsesX;
+int NpulsesY;
+int pulsesX, pulsesY;
+int startX = 100, startY = 100;
+void setup () {
+  // set the window size:
+  size(500, 500);        
+  background(0);
+  // List all the available serial ports
+  println(Serial.list());// look in the window when the program runs to see which port to use
+  myPort = new Serial(this, Serial.list()[0], 115200);  // for me com 26 was port 0, so I put a 0 here
+  
+  // don't generate a serialEvent() unless you get a newline character:
+  myPort.bufferUntil('\n');
+  // set inital background:
+}
+void draw () {
+  
+  if (pulses < 5000) {
+      pulsesX = pulses + startX;
+    }
+    
+  if (pulses > 5000) {
+      pulsesY = (pulses + startY)- 10000;
+    }
+  stroke(255);
+  
+  line(NpulsesX, NpulsesY, pulsesX, pulsesY);
+  
+  NpulsesX = pulsesX;
+  NpulsesY = pulsesY;
+}
+
+void serialEvent (Serial myPort) {// triggers when new data comes in
+
+  String pulses_raw = myPort.readStringUntil('\n'); //read until the new line, like print ln
+
+  if (pulses_raw != null) {
+    // trim off any whitespace:
+    pulses_raw = trim(pulses_raw);
+    // convert to an int and map to the screen height:
+    pulses = int(pulses_raw); 
+ }
+}
+
+
---
+++ Code/Sketch_Knobs/Sketch_Knobs.ino
@@ -0,0 +1,118 @@
+int pulses, pulsesB=10000, A_SIG=0, B_SIG=1, AB_SIG=0, BB_SIG=1;
+
+void setup(){
+  attachInterrupt(0, A_RISE, RISING);
+  attachInterrupt(1, B_RISE, RISING);
+  attachInterrupt(4, AB_RISE, RISING);
+  attachInterrupt(5, BB_RISE, RISING);
+  Serial.begin(115200);
+}//setup
+
+
+void loop(){
+    
+}
+
+void A_RISE(){
+ detachInterrupt(0);
+ A_SIG=1;
+ 
+ if(B_SIG==0)
+ pulses++;//moving forward
+ if(B_SIG==1)
+ pulses--;//moving reverse
+ //Serial.println("Right: ");
+ Serial.println(pulses);
+ attachInterrupt(0, A_FALL, FALLING);
+}
+
+void A_FALL(){
+  detachInterrupt(0);
+ A_SIG=0;
+ 
+ if(B_SIG==1)
+ pulses++;//moving forward
+ if(B_SIG==0)
+ pulses--;//moving reverse
+ //Serial.println("Right: ");
+ Serial.println(pulses);
+ attachInterrupt(0, A_RISE, RISING);  
+}
+
+void B_RISE(){
+ detachInterrupt(1);
+ B_SIG=1;
+ 
+ if(A_SIG==1)
+ pulses++;//moving forward
+ if(A_SIG==0)
+ pulses--;//moving reverse
+ //Serial.println("Right: ");
+ Serial.println(pulses);
+ attachInterrupt(1, B_FALL, FALLING);
+}
+
+void B_FALL(){
+ detachInterrupt(1);
+ B_SIG=0;
+ 
+ if(A_SIG==0)
+ pulses++;//moving forward
+ if(A_SIG==1)
+ pulses--;//moving reverse
+ //Serial.println("Right: ");
+ Serial.println(pulses);
+ attachInterrupt(1, B_RISE, RISING);
+}
+
+void AB_RISE(){
+ detachInterrupt(4);
+ AB_SIG=1;
+ 
+ if(BB_SIG==0)
+ pulsesB++;//moving forward
+ if(BB_SIG==1)
+ pulsesB--;//moving reverse
+ //Serial.println("Left: ");
+ Serial.println(pulsesB);
+ attachInterrupt(4, AB_FALL, FALLING);
+}
+
+void AB_FALL(){
+  detachInterrupt(4);
+ AB_SIG=0;
+ 
+ if(BB_SIG==1)
+ pulsesB++;//moving forward
+ if(BB_SIG==0)
+ pulsesB--;//moving reverse
+ //Serial.println("Left: ");
+ Serial.println(pulsesB);
+ attachInterrupt(4, AB_RISE, RISING);  
+}
+
+void BB_RISE(){
+ detachInterrupt(5);
+ BB_SIG=1;
+ 
+ if(AB_SIG==1)
+ pulsesB++;//moving forward
+ if(AB_SIG==0)
+ pulsesB--;//moving reverse
+ //Serial.println("Left: ");
+ Serial.println(pulsesB);
+ attachInterrupt(5, BB_FALL, FALLING);
+}
+
+void BB_FALL(){
+ detachInterrupt(5);
+ BB_SIG=0;
+ 
+ if(AB_SIG==0)
+ pulsesB++;//moving forward
+ if(AB_SIG==1)
+ pulsesB--;//moving reverse
+ //Serial.println("Left: ");
+ Serial.println(pulsesB);
+ attachInterrupt(5, BB_RISE, RISING);
+}
